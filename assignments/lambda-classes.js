1ff76dfe4fb795a64d11e35c6f80bca115d5c375
joyacamil <deshaunarainer@gmail.com>
2019-08-16 13:11:48 -0400

    instructor created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,18 @@ class Person {
     console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
   }
 }
+
+class Instructor extends Person {
+  constructor(instructorAttr){
+    super(instructorAttr);
+    this.specialty = instructorAttr.specialty;
+    this.favLanguage = instructorAttr.favLanguage
+    this.catchPhrase = instructorAttr.catchPhrase;
+   }  
+  demo(subject) {
+   console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student,subject){
+    console.log(`${student.name} receives a perfect score on ${subject}`)
+  }
+}
